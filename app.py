941e82bc5063685d6b6f331523c61f3eca20df55
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-23 20:40:14 -0300

    Create function mostrar_menu()

---
+++ app.py
@@ -0,0 +1,12 @@
+def mostrar_menu():
+    print('--- Mini Controle Acadêmico ---')
+    print('1. Cadastrar disciplina')
+    print('2. Pesquisar disciplina')
+    print('3. Listar disciplinas cadastradas')
+    print('4. Cadastrar professor em disciplina')
+    print('5. Matricular aluno em disciplina')
+    print('6. Lançar notas do aluno em uma disciplina')
+    print('7. Listar alunos de uma disciplina')
+    print('8. Listra notas dos alunos de uma disciplina')
+    print('9. Sair')
+    print()
